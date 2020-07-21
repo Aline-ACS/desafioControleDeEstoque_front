e1823f4d1fe534af6bfaf68a76dc156686ab9612
Aline-Acs <aline_capoani@hotmail.com>
2020-07-20 22:33:09 -0300

    no message

--- src/index.js
+++ src/index.js
@@ -59,23 +59,6 @@ class Product {
         })
     }
 
-    // layoutProduct(name, brand, quantity, perishable, id) {
-    //     return `
-    //          <div class="products">
-    //              <div class="border border-dark rounded card mb-3">
-    //                 <ul class="list-group list-group-flush">
-    //                      <div class="card-header"> Produto: ${name}</div>
-    //                      <li class="list-group-item">Marca: ${brand}</li>
-    //                      <li class="list-group-item">Quantidade: ${quantity}</li>
-    //                      <li class="list-group-item">Perecível:${perishable}</li>
-    //                      <button type="button" class="btn btn-outline-danger deleteProduct" id="${id}">Excluir</button>
-    //                      <button type="button" class="btn btn-outline-success getProduct" id="${id}" data-toggle="modal" data-target="#modalProduct">Editar</button>
-    //                 </ul>
-    //              </div>
-    //          </div>
-    //     `; 
-    // }
-
     layoutProduct(name, brand, quantity, perishable, id) {
        if(perishable === true) {
             return `
